06afe6fffb5e705d20356b8e571197ef1050545b
jddeltoro <jdiazdeltoro@gmail.com>
2022-05-03 00:03:13 -0400

    add counter example

--- src/UseState/StateTutorial.js
+++ src/UseState/StateTutorial.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
 const StateTutorial = () => {
-  const [inputValue, setInputValue] = useState("Pedro");
+  const [inputValue, setInputValue] = useState("This information will be changed");
+  const [counter, setCounter] = useState(0);
 
-  let onChange = (event) => {
+  let updateUiInformation = (event) => {
     const newValue = event.target.value;
     setInputValue(newValue);
   };
 
   return (
     <div>
-      <input placeholder="enter something..." onChange={onChange} />
-      {inputValue}
+      <div>
+        <input placeholder="enter something..." onChange={updateUiInformation} />
+        {inputValue}
+      </div>
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>Click Me {counter}</button>
+      </div>
     </div>
   );
 };
