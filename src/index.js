793e739f0ccab37b38b63151e5345048f11f819c
jddeltoro <jdiazdeltoro@gmail.com>
2022-05-18 22:53:38 -0400

    impor jequery and popper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
